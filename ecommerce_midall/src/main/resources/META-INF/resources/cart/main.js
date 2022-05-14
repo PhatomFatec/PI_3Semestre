bee02c747eaa0b7f4c6ac9c6fde3ce4b0274dc3c
Igor Ribeiro <igor_rsilva97@hotmail.com>
2022-05-14 02:10:22 -0300

    Update carrinho

--- ecommerce_midall/src/main/resources/META-INF/resources/cart/main.js
+++ ecommerce_midall/src/main/resources/META-INF/resources/cart/main.js
@@ -18,21 +18,38 @@ document.getElementById('btn-Cancelar')
 
 function funcaoSalvar() {
 
-	//let nomeProduto = document.getElementById('btn-Salvar').value;
-	//document.getElementById('colunaNome').innerText = nomeProduto
-	document.getElementById('colunaNome').innerText = "Calça Jeans"
-	document.getElementById('colunaCodigo').innerText = "001002003"
-	document.getElementById('colunaDescricao').innerText = "Tamanho M | Cor: Preta"
-	document.getElementById('colunaCategoria').innerText = "Moda"
-	document.getElementById('colunaPreco').innerText = "R$79.00"
-	document.getElementById('colunaQuantidade').value = "1"
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:8080/carrinho",
+		data: 'teste',
+		cache: false,
+		success: function(data) {
+			console.log('Get Realizado!')
+			console.log(data)
+			var items = [];
+
+             $.each(data, function(i){
 
 
-	document.getElementById('span-precoBruto').innerText = "R$79.00"
-	document.getElementById('span-precoDesconto').innerText = "R$10.00"
-	document.getElementById('span-precoTotal').innerText = "R$69.00"
+                items.push("<tr><td><span" + this.nomeProd + "></span></td>" + "<tr><td><span" + this.codProd + "></span></td></tr>");
+            });
+            $("#cleiton").append(items);
+            
+            
+			document.getElementById('colunaNome').innerText = data[0]['nomeProd']
+			document.getElementById('colunaCodigo').innerText = data[0]['codProd']
+			document.getElementById('colunaDescricao').innerText = data[0]['descProd']
+			document.getElementById('colunaCategoria').innerText = data[0]['categoria']
+			document.getElementById('colunaPreco').innerText = "R$ " + data[0]['valorProd']
+			//document.getElementById('colunaQuantidade').value = "1"
+
+	
+			document.getElementById('span-precoBruto').innerText = "R$79.00"
+			document.getElementById('span-precoDesconto').innerText = "R$10.00"
+			document.getElementById('span-precoTotal').innerText = "R$69.00"
+		}
+	});
 }
-
 function listarProduto() {
 
 	$.ajax({
@@ -40,31 +57,43 @@ function listarProduto() {
 		url: "http://localhost:8080/carrinho",
 		data: 'teste',
 		cache: false,
-		success: function(data)  {
+		success: function(data) {
 			console.log('Get Realizado!')
 			console.log(data)
-
-			document.getElementById('span-nome').innerText = data[0]['nomeProd']
-			document.getElementById('span-categoria').innerText = data[0]['categoria']
-			document.getElementById('span-preco').innerText = "R$" + data[0]['valorProd']
-
-			document.getElementById('span-nome1').innerText = data[1]['nomeProd']
-			document.getElementById('span-categoria1').innerText = data[1]['categoria']
-			document.getElementById('span-preco1').innerText = "R$" + data[1]['valorProd']
 			
-			document.getElementById('span-nome2').innerText = data[2]['nomeProd']
-			document.getElementById('span-categoria2').innerText = data[2]['categoria']
-			document.getElementById('span-preco2').innerText = "R$" + data[2]['valorProd']
+			var items = [];
 
-			document.getElementById('span-nome3').innerText = data[3]['nomeProd']
-			document.getElementById('span-categoria3').innerText = data[3]['categoria']
-			document.getElementById('span-preco3').innerText = "R$" + data[3]['valorProd']
+             $.each(data, function(i){
 
-			document.getElementById('span-nome4').innerText = data[4]['nomeProd']
-			document.getElementById('span-categoria4').innerText = data[4]['categoria']
-			document.getElementById('span-preco4').innerText = "R$" + data[4]['valorProd']
 
+                items.push("<tr><th><b> Nome: " + this.nomeProd + "</span>&nbsp;</th>" + "<tr><th><b> Categoria: " + this.categoria + "</span>&nbsp;</th>" +"<tr><th><b> Preço: R$" + this.valorProd + "</span>&nbsp;</th></tr></b>");
+            });
+            $("#douglas").append(items);
+           
 		}
 
+	});
+}
+
+function funcaoAtualizar() {
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:8080/carrinho",
+		data: 'teste',
+		cache: false,
+		success: function(data) {
+			console.log('Get Realizado!')
+			console.log(data)
+			let quantidade = parseInt(document.getElementById('colunaQuantidade').value)
+			let valorPreco = document.getElementById('colunaPreco').innerHTML
+			valorPreco = valorPreco.replace('R$', '')
+			valorPreco = parseInt(valorPreco)
+
+			let precoTotal = (quantidade * valorPreco)
+			console.log(precoTotal)
+			console.log(typeof (precoTotal))
+			document.getElementById('colunaPreco').innerText = "R$ " + precoTotal
+
+		}
 	});
 }
